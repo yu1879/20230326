62270aff4ef8e499f4dddfab6a66d5891669545b
yu1879 <yu1879@yu1879-PC>
2023-04-30 15:39:12 +0800

    spring

---
+++ src/main/java/spring/core/session05/aop_lab/Dancer.java
@@ -0,0 +1,12 @@
+package spring.core.session05.aop_lab;
+
+public class Dancer implements Performance {
+
+	@Override
+	public void perform() {
+		// TODO Auto-generated method stub
+		System.out.println("舞者表演開始:跳舞、轉圈、翻滾....");
+		System.out.println("舞者表演結束");
+	}
+
+}
---
+++ src/main/java/spring/core/session05/aop_lab/Performance.java
@@ -0,0 +1,5 @@
+package spring.core.session05.aop_lab;
+
+public interface Performance {
+	public void perform();
+}
