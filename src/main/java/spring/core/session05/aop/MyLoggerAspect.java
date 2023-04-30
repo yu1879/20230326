fb4ff78d841fd4af2f3c15349dd4af71fdb2ae00
yu1879 <yu1879@yu1879-PC>
2023-04-30 14:04:22 +0800

    spring

--- src/main/java/spring/core/session05/aop/MyLoggerAspect.java
+++ src/main/java/spring/core/session05/aop/MyLoggerAspect.java
@@ -1,5 +1,9 @@
 package spring.core.session05.aop;
 
+import java.util.Arrays;
+
+import org.aopalliance.intercept.Joinpoint;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.core.annotation.Order;
@@ -9,8 +13,10 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Order(1)
 public class MyLoggerAspect {
-	@Before(value = "execution(public Integer spring.core.session05.aop.CalcImpl.add(Integer,Integer))")
-	public void beforeAdvice() {
-		System.out.println("呼叫前置通知");
+	@Before(value = "execution(* *(..))")
+	public void beforeAdvice(JoinPoint joinpoint) {
+		String methodName = joinpoint.getSignature().getName();
+		Object[] args = joinpoint.getArgs();
+		System.out.printf("呼叫前置通知-方法名稱:%s 方法參數:%s\n", methodName, Arrays.toString(args));
 	}
 }
