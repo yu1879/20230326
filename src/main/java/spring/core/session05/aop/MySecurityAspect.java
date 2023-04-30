6a26f0a9c98481179362b9ba1d658159d025ca2f
yu1879 <yu1879@yu1879-PC>
2023-04-30 15:29:54 +0800

    spring

--- src/main/java/spring/core/session05/aop/MySecurityAspect.java
+++ src/main/java/spring/core/session05/aop/MySecurityAspect.java
@@ -1,5 +1,7 @@
 package spring.core.session05.aop;
 
+import java.util.Arrays;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -20,12 +22,17 @@ public class MySecurityAspect {
 	@Around(value = "pt()")
 	public Object aroundAdvice(ProceedingJoinPoint joinPoint) {
 		Object result = null;
-
 		try {
+			String methodName = joinPoint.getSignature().getName();
+			Object[] args = joinPoint.getArgs();
+			System.out.printf("環繞通知(前置通知) - 方法名稱：%s 方法參數：%s\n", methodName, Arrays.toString(args));
 			result = joinPoint.proceed();
+			System.out.printf("環繞通知(返回通知) - result：%s\n", result);
+
 		} catch (Throwable ex) {
-			// TODO: handle exception
+			System.out.printf("環繞通知(異常通知) - ex：%s\n", ex);
 		} finally {
+			System.out.printf("環繞通知(後置通知)\n");
 
 		}
 		return result;
