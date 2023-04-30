12776abe72a291c2d9d92646ff595fca84694c84
yu1879 <yu1879@yu1879-PC>
2023-04-30 15:16:46 +0800

    spring

---
+++ src/main/java/spring/core/session05/aop/ComputerImpl.java
@@ -0,0 +1,20 @@
+package spring.core.session05.aop;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComputerImpl implements Computer {
+
+	@Override
+	public Integer add(Integer x, Integer y) {
+		int result = x + y;
+		return result;
+	}
+
+	@Override
+	public Integer div(Integer x, Integer y) {
+		int result = x / y;
+		return result;
+	}
+
+}
---
+++ src/main/java/spring/core/session05/aop/MySecurityAspect.java
@@ -0,0 +1,33 @@
+package spring.core.session05.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(2)
+
+public class MySecurityAspect {
+	@Pointcut(value = "execution(* spring.core.session05.aop.ComputerImpl.*(..))")
+	public void pt() {
+
+	}
+
+	@Around(value = "pt()")
+	public Object aroundAdvice(ProceedingJoinPoint joinPoint) {
+		Object result = null;
+
+		try {
+
+		} catch (Throwable ex) {
+			// TODO: handle exception
+		} finally {
+
+		}
+		return result;
+	}
+}
