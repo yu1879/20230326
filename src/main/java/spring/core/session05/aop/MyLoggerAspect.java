2d3c8765fb76213b36c40cb9c4fa378c88473f6b
yu1879 <yu1879@yu1879-PC>
2023-04-30 13:46:27 +0800

    spring

--- src/main/java/spring/core/session05/aop/MyLoggerAspect.java
+++ src/main/java/spring/core/session05/aop/MyLoggerAspect.java
@@ -1,6 +1,7 @@
 package spring.core.session05.aop;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -8,5 +9,8 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Order(1)
 public class MyLoggerAspect {
-
+	@Before(value = "execution(public Integer spring.core.session05.aop.CalcImpl.add(Integer,Integer))")
+	public void beforeAdvice() {
+		System.out.println("呼叫前置通知");
+	}
 }
