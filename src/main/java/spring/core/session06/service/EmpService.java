3913133c2c59e4165d1f7dc2b3022912caff9cfb
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-07 16:45:58 +0800

    spring

---
+++ src/main/java/spring/core/session06/service/EmpService.java
@@ -0,0 +1,12 @@
+package spring.core.session06.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import spring.core.session06.jdbc.entity.Emp;
+
+public interface EmpService {
+	public List<Emp> findAllEmps();
+
+	public Integer getMaxOfAge();
+}
---
+++ src/main/java/spring/core/session06/service/EmpServiceImpl.java
@@ -0,0 +1,27 @@
+package spring.core.session06.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import spring.core.session06.jdbc.dao.EmpDao;
+import spring.core.session06.jdbc.entity.Emp;
+
+public class EmpServiceImpl implements EmpService {
+@Autowired
+private EmpDao empDao;
+	@Override
+	public List<Emp> findAllEmps() {
+		// TODO Auto-generated method stub
+		return empDao.queryEmps();
+	}
+
+
+	@Override
+	public Integer getMaxOfAge() {
+List<Emp> emps=findAllEmps();
+return emps.stream().mapToInt(Emp::getAge).findAny().getAsInt();
+	}
+
+}
