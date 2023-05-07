68f663a7cd6e3e4ad0ef94d8eab28dbd5b96e3bb
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-07 22:15:00 +0800

    spring

--- src/main/java/spring/core/session06/controller/EmpController.java
+++ src/main/java/spring/core/session06/controller/EmpController.java
@@ -1,5 +1,27 @@
 package spring.core.session06.controller;
 
+import java.util.List;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import spring.core.session06.jdbc.dao.EmpDao;
+import spring.core.session06.jdbc.entity.Emp;
+import spring.core.session06.service.EmpService;
+
+@Controller
 public class EmpController {
+	@Autowired
+	private EmpService empService;
+
+	public List<Emp> queryEmps() {
+		return empService.findAllEmps();
+	}
+
+	public Integer getMaxOfAge() {
+		return empService.getMaxOfAge();
+	}
 
 }
--- src/main/java/spring/core/session06/service/EmpServiceImpl.java
+++ src/main/java/spring/core/session06/service/EmpServiceImpl.java
@@ -4,24 +4,25 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import spring.core.session06.jdbc.dao.EmpDao;
 import spring.core.session06.jdbc.entity.Emp;
-
+@Service
 public class EmpServiceImpl implements EmpService {
-@Autowired
-private EmpDao empDao;
+	@Autowired
+	private EmpDao empDao;
+
 	@Override
 	public List<Emp> findAllEmps() {
 		// TODO Auto-generated method stub
 		return empDao.queryEmps();
 	}
 
-
 	@Override
 	public Integer getMaxOfAge() {
-List<Emp> emps=findAllEmps();
-return emps.stream().mapToInt(Emp::getAge).findAny().getAsInt();
+		List<Emp> emps = findAllEmps();
+		return emps.stream().mapToInt(Emp::getAge).max().getAsInt();
 	}
 
 }
