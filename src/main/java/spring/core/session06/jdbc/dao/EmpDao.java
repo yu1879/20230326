ddf068a46b577c393eb3834635ec3a066e061473
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-07 14:24:33 +0800

    spring

---
+++ src/main/java/spring/core/session06/jdbc/dao/EmpDao.java
@@ -0,0 +1,25 @@
+package spring.core.session06.jdbc.dao;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import spring.core.session06.jdbc.entity.Emp;
+
+public interface EmpDao {
+	List<Map<String, Object>> queryAll();
+
+	List<Emp> querEmps();
+
+	Optional<Emp> getOne(Integer eid);
+
+	Integer count();
+
+	int create(String ename, Integer age);
+
+	int[] batchCreate(List<Object[]> list);
+
+	int update(Integer eid, String ename, Integer age);
+
+	int delete(Integer eid);
+}
