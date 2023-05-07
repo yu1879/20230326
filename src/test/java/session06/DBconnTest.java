9dc147302584f4f000231111b5fa7f89847d4d94
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-07 14:33:52 +0800

    spring

--- src/test/java/session06/DBconnTest.java
+++ src/test/java/session06/DBconnTest.java
@@ -5,20 +5,24 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
+import spring.core.session06.jdbc.dao.EmpDao;
+
 public class DBconnTest {
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("jdbc-config.xml");
-
-		ComboPooledDataSource cp = ctx.getBean("dataSource", ComboPooledDataSource.class);
-
-		try {
-			System.out.println(cp.getConnection());
-			System.out.println("連線成功");
-		} catch (Exception e) {
-			// TODO: handle exception
-			System.out.println("連線失敗");
-			e.printStackTrace();
-		}
+		EmpDao empDao = ctx.getBean(EmpDao.class);
+		System.out.println(empDao.queryAll());
+//		ComboPooledDataSource cp = ctx.getBean("dataSource", ComboPooledDataSource.class);
+//
+//		try {
+//			System.out.println(cp.getConnection());
+//			System.out.println("連線成功");
+//		} catch (Exception e) {
+//			// TODO: handle exception
+//			System.out.println("連線失敗");
+//			e.printStackTrace();
+//		}
+//
 
 	}
 }
