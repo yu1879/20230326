4cea3a29356b2314a45060113b0c1d398c251b17
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-14 15:12:45 +0800

    tx

--- src/main/java/spring/core/session07/tx/service/BookManyServiceImpl.java
+++ src/main/java/spring/core/session07/tx/service/BookManyServiceImpl.java
@@ -2,12 +2,14 @@ package spring.core.session07.tx.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class BookManyServiceImpl implements BookManyService {
 	@Autowired
 	private BookOneService bookOneService;
 
+	@Transactional
 	@Override
 	public void buyMany(String username, Integer... bookIds) {
 		// TODO Auto-generated method stub
