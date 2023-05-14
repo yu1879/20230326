a32c034e80c5ad010abe04ece8cb6a3ff7fde383
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-14 13:35:03 +0800

    tx

---
+++ src/main/java/spring/core/session07/tx/dao/BookDao.java
@@ -0,0 +1,11 @@
+package spring.core.session07.tx.dao;
+
+public interface BookDao {
+	Integer getBookPrice(Integer bookId);
+
+	Integer getBookStocket(Integer bookId);
+
+	Integer updateBookStock(Integer bookId);
+
+	Integer updateWallet(String username, Integer price);
+}
